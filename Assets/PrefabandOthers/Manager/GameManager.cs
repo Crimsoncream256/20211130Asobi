4d5d05131003ef8a869f3327220b8c1bfad28731
Crimsoncream256 <90096571+Crimsoncream256@users.noreply.github.com>
2021-11-30 18:16:38 +0900

    initial commit

---
+++ Assets/PrefabandOthers/Manager/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject TitleButton;
+    public GameObject RetlyButton;
+    public GameObject SelectButton;
+    public GameObject NextButton;
+
+    void Start()
+    {
+        TitleButton.SetActive(false);
+        RetlyButton.SetActive(false);
+        SelectButton.SetActive(false);
+        NextButton.SetActive(false);
+    }
+
+    public void ButtonRetly()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
+}
